3ba251eae37297a55f6dba56c3adda9fad548773
L <L@LdeMac-mini.local>
2014-06-21 10:46:46 +0800

    Initial Commit

---
+++ CLBasicFramework/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CLBasicFramework
+//
+//  Created by L on 14-6-21.
+//  Copyright (c) 2014年 L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
